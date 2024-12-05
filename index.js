0e61ebea50ef6e244e38a616735fc6c165c492d6
Zevhys <rakha2728@gmail.com>
2024-12-05 22:16:02 +0800

    add package

--- index.js
+++ index.js
@@ -2,10 +2,31 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8000;
 const path = require("path");
+const cors = require("cors");
+const favicon = require("serve-favicon");
+
+app.use(
+  cors({
+    origin: "http://localhost:8080",
+    methods: ["GET", "POST", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+
+app.use((req, res, next) => {
+  res.cookie("exampleCookie", "value", {
+    httpOnly: true,
+    secure: true,
+    sameSite: "None",
+  });
+  next();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/img", express.static(path.join(__dirname, "/img")));
+app.use(favicon(path.join(__dirname, "favicon.ico")));
 
 const db = require("./app/models");
 db.mongoose
