00a987f9779ad63fa7d2d3eac609976fd3aec8fc
Zevhys <rakha2728@gmail.com>
2024-12-05 20:58:32 +0800

    create endpoint to get one data product

--- app/controllers/product.controller.js
+++ app/controllers/product.controller.js
@@ -10,3 +10,15 @@ exports.findAll = (req, res) => {
       res.status(500).send({ message: err.message });
     });
 };
+
+exports.findOne = (req, res) => {
+  Product.findOne({
+    code: req.params.id,
+  })
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      res.status(500).send({ message: err.message });
+    });
+};
